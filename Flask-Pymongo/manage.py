c7157bb04f5593d13af0dbae98e7b32a15b66b20
Scof <32575460+songyw0517@users.noreply.github.com>
2022-01-27 05:06:04 +0900

    Update manage.py

--- Flask-Pymongo/manage.py
+++ Flask-Pymongo/manage.py
@@ -7,10 +7,15 @@ flask application 객체를 생성하고, 각종 shell context 설정 및 cli co
 
 from config import config
 from app import create_app
-import click
 application = create_app(config)
 
-# cli-test 명령어 등록
+# cli 명령어 등록
+# flask_app 등록후 사용가능
 @application.cli.command('cli-test')
 def cli_test():
     print('cli test')
+
+# # db를 초기화하는 명령어
+# @application.cli.command('db-init')
+# def db_init():
+    
